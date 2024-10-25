aa34065ea78b3496b953a89f9bb342d0e22a5dfc
Marina <marinaakter878@gmail.com>
2024-10-25 09:39:47 +0600

    country component to include visited status and code display

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
 import "./Country.css"
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
     const {name, flags, population, area, cca3} = country
 
     const [visited, setVisited] = useState(false)
     const handleVisited = () =>{
         setVisited(!visited)
-    }
+    }    
     
   return (
     <div className={`country ${visited && 'visited'}`}>
@@ -15,6 +15,8 @@ const Country = ({country}) => {
       <p>Population: {population} </p>
       <p>Area: {area}</p>
       <p><small>Code: {cca3} </small></p>
+      <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+      <br /> 
       <button onClick={handleVisited}>{visited? 'Visited':'Going'}</button>
       {visited && 'I have visited this country.'}
     </div>
